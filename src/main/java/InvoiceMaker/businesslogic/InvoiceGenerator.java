a95f0b0888688c47a543352adb3ee3634922f99f
Julien <zigan.julien@gmail.com>
2024-10-17 09:49:02 +0200

    add header format

--- src/main/java/InvoiceMaker/businesslogic/InvoiceGenerator.java
+++ src/main/java/InvoiceMaker/businesslogic/InvoiceGenerator.java
@@ -3,8 +3,10 @@ package InvoiceMaker.businesslogic;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import java.awt.*;
 import java.io.File;
@@ -14,29 +16,63 @@ public class InvoiceGenerator {
         String filepath = "";
 
         try (PDDocument document = new PDDocument()) {
-            PDPage page = new PDPage();
+            PDPage page = new PDPage(PDRectangle.A4);
             document.addPage(page);
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
-            PDType1Font font = new PDType1Font(Standard14Fonts.FontName.HELVETICA);
+            float xOffsetTracker = 0;
+            float yOffsetTracker = 0;
+
+
+
+            PDImageXObject pdImage = PDImageXObject.createFromFile("X:\\invoice-maker\\src\\main\\resources\\logo1.PNG", document);
+            contentStream.drawImage(pdImage, 380, 715);
 
             contentStream.beginText();
-            contentStream.setFont(font, 12);
-            contentStream.newLineAtOffset(250, 700);
-            contentStream.showText("Rechnung");
+            contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 8);
+            contentStream.newLineAtOffset(page.getMediaBox().getWidth() / 100 * 12, page.getMediaBox().getHeight() / 1000 * 806);
+            xOffsetTracker += page.getMediaBox().getWidth() / 100 * 12;
+            yOffsetTracker += page.getMediaBox().getHeight() / 1000 * 806;
+            contentStream.showText("Anita Pacholik-Zuromska | Friedrich-Wilhelm-Str. 46A | 12103 Berlin");
 
-            contentStream.newLineAtOffset(-150, -100);
-            RecipientCompositor.compose(contentStream, einsatzbestaetigung.getRecipient().testrecip);
+            contentStream.setFont(new PDType1Font((Standard14Fonts.FontName.HELVETICA_BOLD)), 11);
+            contentStream.newLineAtOffset(0,  -(page.getMediaBox().getHeight() / 1000 * 27));
+            yOffsetTracker -= page.getMediaBox().getHeight() / 1000 * 27;
+            contentStream.showText("Charite");
 
-            contentStream.newLineAtOffset(0, -40);
-            contentStream.showText("Rechnungsnummer: " + einsatzbestaetigung.getAuftragsNr().getAUFTRAGSNR());
+            contentStream.setFont(new PDType1Font((Standard14Fonts.FontName.HELVETICA)), 10);
+            contentStream.newLineAtOffset(0,  -(page.getMediaBox().getHeight() / 1000 * 17));
+            yOffsetTracker -= page.getMediaBox().getHeight() / 1000 * 17;
+            contentStream.showText("Finanz- und Rechnungswesen");
+
+            contentStream.setFont(new PDType1Font((Standard14Fonts.FontName.HELVETICA)), 10);
+            contentStream.newLineAtOffset(0,  -(page.getMediaBox().getHeight() / 1000 * 16));
+            yOffsetTracker -= page.getMediaBox().getHeight() / 1000 * 16;
+            contentStream.showText("Hindenburgdamm 30");
+
+            contentStream.setFont(new PDType1Font((Standard14Fonts.FontName.HELVETICA)), 10);
+            contentStream.newLineAtOffset(0,  -(page.getMediaBox().getHeight() / 1000 * 16));
+            yOffsetTracker -= page.getMediaBox().getHeight() / 1000 * 16;
+            contentStream.showText("12200 Berlin");
+
+            contentStream.setFont(new PDType1Font((Standard14Fonts.FontName.HELVETICA_BOLD)), 10);
+            contentStream.newLineAtOffset(page.getMediaBox().getWidth() / 1000 * 465,  -(page.getMediaBox().getHeight() / 1000 * 58));
+            xOffsetTracker += page.getMediaBox().getWidth() / 1000 * 465;
+            yOffsetTracker -= page.getMediaBox().getHeight() / 1000 * 58;
+            contentStream.showText("Leistungsdatum");
+
+            contentStream.setFont(new PDType1Font((Standard14Fonts.FontName.HELVETICA_BOLD)), 10);
+            contentStream.newLineAtOffset(page.getMediaBox().getWidth() / 1000 * 159,  0);
+            xOffsetTracker += page.getMediaBox().getWidth() / 1000 * 159;
+            contentStream.showText("Rechnungsdatum");
+
+
             contentStream.endText();
-
             contentStream.close();
 
             filepath = "X:\\Einsatzverwaltung\\Einsatzverwaltung\\target\\generated-sources\\output\\testFile1.pdf";
 
             document.save(filepath);
-//            System.out.println("PDF created successfully.");
+            System.out.println("PDF created successfully.");
 //            File file = new File("X:\\Einsatzverwaltung\\Einsatzverwaltung\\target\\generated-sources\\output\\testFile1.pdf");
 //            Desktop.getDesktop().open(file);
 
@@ -47,32 +83,8 @@ public class InvoiceGenerator {
     }
 
     public static void generateFinal(Einsatzbestaetigung einsatzbestaetigung) {
-        String filepath = "";
-
-        try (PDDocument document = new PDDocument()) {
-            PDPage page = new PDPage();
-            document.addPage(page);
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-            PDType1Font font = new PDType1Font(Standard14Fonts.FontName.HELVETICA);
-
-            contentStream.beginText();
-            contentStream.setFont(font, 12);
-            contentStream.newLineAtOffset(250, 700);
-            contentStream.showText("Rechnung");
-
-            contentStream.newLineAtOffset(-150, -100);
-            RecipientCompositor.compose(contentStream, einsatzbestaetigung.getRecipient().testrecip);
-
-            contentStream.newLineAtOffset(0, -40);
-            contentStream.showText("Rechnungsnummer: " + einsatzbestaetigung.getAuftragsNr().getAUFTRAGSNR());
-            contentStream.endText();
-
-            contentStream.close();
-
-            filepath = "X:\\Einsatzverwaltung\\Einsatzverwaltung\\target\\generated-sources\\output\\testFile1.pdf";
-
-            document.save(filepath);
-            System.out.println("PDF created successfully.");
+        try {
+            generate(einsatzbestaetigung);
             File file = new File("X:\\Einsatzverwaltung\\Einsatzverwaltung\\target\\generated-sources\\output\\testFile1.pdf");
             Desktop.getDesktop().open(file);
 
